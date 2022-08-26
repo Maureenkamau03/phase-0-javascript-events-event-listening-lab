1099f01a9a7d95d8ac6d5423051652dacbf41c81
Maureenkamau03 <mkaymaureen@gmail.com>
2022-08-26 04:32:39 +0300

    completed assignmet

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ let input = document.getElementById("button");
 function addingEventListener() {
 	input.addEventListener("click", addingEventListener);
 }
+
+// function addingEventListener() {
+// 	const input = document.getElementById("input");
+
+// 	function clickAlert() {
+// 		alert("I was clicked!");
+// 	}
+
+// 	input.addEventListener("click", clickAlert);
+// }
+// addingEventListener();
